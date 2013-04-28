6d1511d47a991dcec8cadac03babb03a6d0266bf
Devin Gaffney <itsme@devingaffney.com>
2013-04-28 16:50:08 -0700

    internet

--- extract_gexf_data.rb
+++ extract_gexf_data.rb
@@ -3,10 +3,13 @@ require 'hashie'
 require 'nokogiri'
 require 'csv'
 ["cpc", "ndp"].each do |party|
+  party = "cpc"
   print "."
-  results = GephiExtractor.run({:gexf_network => File.read("data/graphs/#{party}_top_by_eigen_removed.gexf")})
+  results = GephiExtractor.run({:gexf_network => File.read("data/graphs/#{party}_updates.gexf")})
   print "."
-  node_csv = CSV.open("data/csv/#{party}_new_nodes_extracted.csv", "w")
+  interaction_counts = Hash[CSV.read("data/csv/#{party}_mention_counts.csv")]
+  knowledge_counts = Hash[CSV.read("data/csv/#{party}_users_term_scores.csv").collect{|r| [r.first,r.last]}]  
+  node_csv = CSV.open("data/csv/#{party}_nodes_extracted.csv", "w")
   node_keys = [:id, :label, :size]
   node_attributes = ["statuses_count", "followers_count", "friends_count", "listed_count", "favourites_count", "klout_score", "klout_score_bucket", "kred_influence", "kred_outreach", "indegree", "outdegree", "degree", "weighted degree", "weighted indegree", "weighted outdegree", "eccentricity", "closnesscentrality", "betweenesscentrality", "eigencentrality", "newClusteringCoefficient", "clustering", "componentnumber", "strongcompnum", "pageranks", "authority", "hub", "modularity_class"]
   rows = []
@@ -18,6 +21,8 @@ require 'csv'
     node_attributes.each do |attribute|
       row[attribute] = node[:attributes].select{|x| x[:for] == attribute}.first[:value] if node[:attributes].select{|x| x[:for] == attribute}.first
     end
+    row[:interaction_count] = interaction_counts[row[:id]]
+    row[:knowledge_count] = knowledge_counts[row[:id]]
     rows << row
   end
   node_csv << (node_keys|node_attributes).collect(&:to_sym)
